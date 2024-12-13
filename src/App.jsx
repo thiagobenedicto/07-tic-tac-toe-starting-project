9a10b0f8fc09cbf452a4df0aa8a2bc7646c807ca
Thiago Benedicto <benedict.thiago@gmail.com>
2024-12-13 01:45:44 -0300

    feat: lifting state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
+import {useState} from 'react';
 import GameBoard from './components/GameBoard';
 import Player from './components/Player';
 
 function App() {
+  // Elevando o state
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePlayer((currentPlayer) => (currentPlayer === 'X' ? 'O' : 'X'));
+  }
+
   return (
     <main>
       <div id='game-container'>
-        <ol id='players'>
-          <Player initialName='Player 1' symbol='X' />
-          <Player initialName='Player 2' symbol='O' />
+        <ol id='players' className='highlight-player'>
+          <Player
+            initialName='Player 1'
+            symbol='X'
+            isActive={activePlayer === 'X'}
+          />
+          <Player
+            initialName='Player 2'
+            symbol='O'
+            isActive={activePlayer === 'O'}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
       LOGS
     </main>
